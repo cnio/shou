2db0b6cba66f6d04204342c9cb9ded577757c711
shilong.sang <i5ting@126.com>
2017-04-26 14:28:34 +0800

    lazycommmit, please ignore

--- README.md
+++ README.md
@@ -7,14 +7,9 @@
 - 其实更多的，你只关系你的package.json的dependency模块
 - 比如依赖babel，你的node_modules就要有100多个模块
 
-## 步骤
+## 实现步骤
 
 - 读取pwd下面的package.json
 - 获取依赖的，把非依赖的模块都放到node_modules/cache下面
-- 创建.npmrc 或追加
-
-> cache='./node_modules/cache'%
-
-- 
-
-
+- 创建.npmrc或追加`cache='./node_modules/cache'`
+- 将node_modules/cache下面的模块都以软连接的形式放到~/node_modules下面
--- test.js
+++ test.js
@@ -1,18 +1,4 @@
 
-var fs = require('fs')
-var argv = process.argv;
-argv.shift();
+var userHome = require('user-home');
 
-var file_path = __dirname;
-var current_path = process.cwd();
-
-var node_modules_folder = current_path + "/node_modules"
-
-const fsextra = require('fs-extra')
-
-// remove file
-fsextra.remove(node_modules_folder+'/cache/', err => {
-  if (err) return console.error(err)
-
-  console.log('success!')
-})
+console.log(userHome);
